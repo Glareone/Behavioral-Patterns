be61ba02d92bc7a83e4a4a6245785d53e9ded471
Aliaksei Kalesnikau <aliaksei.kalesnikau@sorted.com>
2019-04-17 14:44:46 +0300

    mediator implementation is finished. Was grabbed and refactored from my previous project(4y old) .

---
+++ DesignPatterns/Mediator/Executor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Mediator
+{
+    /// <summary>
+    ///     Concrete executor
+    ///     Конкретный исполнитель
+    /// </summary>
+    public class Executor
+    {
+        public Executor(string executorName, string executorGroup, string execute)
+        {
+            ExecutorName = executorName;
+            ExecutorGroup = executorGroup;
+            Execute = execute;
+        }
+
+        /// <summary>
+        ///     Prevents a default instance of the <see cref="Executor" /> class from being created.
+        /// </summary>
+        private Executor()
+        {
+        }
+
+        /// <summary>
+        ///     Gets the Execute.
+        /// </summary>
+        /// <value>
+        ///     The dosomething.
+        /// </value>
+        private string Execute { get; }
+
+        /// <summary>
+        ///     Gets or sets the name of the executor.
+        /// </summary>
+        /// <value>
+        ///     The name of the executor.
+        /// </value>
+        private string ExecutorName { get; }
+
+        /// <summary>
+        ///     Gets or sets the executor group.
+        /// </summary>
+        /// <value>
+        ///     The executor path.
+        /// </value>
+        private string ExecutorGroup { get; }
+
+        /// <summary>
+        ///     Does something.
+        /// </summary>
+        public void DoSomething()
+        {
+            Console.WriteLine($"{ExecutorName}, {ExecutorGroup} : {Execute}, {DateTime.Now}");
+        }
+    }
+}
---
+++ DesignPatterns/Mediator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mediator
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var executor1 = new Executor("Glareone", @"Group of usage 1", "Executor Glareone starts to kill'em all");
+            var executor2 = new Executor("Lis", @"Group of usage 1", "Executor Lissan starts to reallocate to US");
+            var executor3 = new Executor("Hell", @"Group of usage 1", "Executor Hell repairs his apartments");
+            var executor4 = new Executor("Arthur", @"Group of usage 2", "Executor Arthur establishes company");
+
+            var executors = new List<Executor> { executor1, executor2, executor3 };
+
+            Mediator.AddExecutors("Mad_Executors", executors);
+            Mediator.AddExecutors("Beer_Executors", new List<Executor> { executor4 });
+
+            Mediator.DoByAllExecutors("Mad_Executors");
+            Console.WriteLine($"amoung of executors is {Mediator.GetExecutors("Mad_Executors").Count}");
+
+            Mediator.DoByAllExecutors("Beer_Executors");
+            Console.WriteLine($"amoung of executors is {Mediator.GetExecutors("Beer_Executors").Count}");
+
+            Console.ReadKey();
+        }
+    }
+}
